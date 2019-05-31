04a24e5e08e8f3b77ca5bac8e438ac6658c72764
Vincent Chou <vin.chou@mail.utoronto.ca>
2019-05-30 23:50:06 -0400

    Added sorting by clicking on column headers.

--- data_display/views.py
+++ data_display/views.py
@@ -2,10 +2,10 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.contrib.staticfiles import finders
 from data_display.models import Students, Teams, Projects, NotForProfits
-from data_display.utils import string_display, dummy_data
+from data_display.utils import string_display
 
 # TODO: There should be a native app context that Django offers. Store everything we store here there instead.
-app_context = {'existing_query_params': "Students", 'pagination_width': 2}
+app_context = {'last_table': "", 'pagination_width': 2, 'last_data': [], 'last_headers': []}
 # this will be changed via settings view in the future
 RESULTS_PER_PAGE = 25
 
@@ -15,25 +15,31 @@ def database_start_page(request):
     # Add string display to our cache
     string_display.cache_display_strings(finders.find('string_conversion.json'), app_context)
 
-    # Setting up using models to generate table data instead, defaulting to Students
-    # TODO: replace query parameters with django form data, will make pagination logic less confusing
-    # TODO: remove additional or existing query params after django forms
-    existing_query_params = app_context['existing_query_params']
-    new_query_params = request.GET.get('tables') or existing_query_params
+    # Check for all the query parameters
+    sort_by = request.GET.get('sort')
+    page_number = request.GET.get('page')
+    table = request.GET.get('table') or 'Students'
 
-    data, table_headers = get_objects_by_table(new_query_params)
+    if sort_by:
+        data, table_headers = get_objects_by_table_and_sort(table,
+                                                            string_display.get_strings_from_cache([sort_by],
+                                                                                                  app_context)[0])
+        app_context['last_data'], app_context['last_headers'] = data, table_headers
+    elif not page_number or not app_context['last_data']:
+        data, table_headers = get_objects_by_table(table)
+        page_number = 1
+        app_context['last_data'], app_context['last_headers'] = data, table_headers
+    else:
+        # This is the pagination case, just use the existing data.
+        data, table_headers = app_context['last_data'], app_context['last_headers']
 
-    app_context['existing_query_params'] = new_query_params
     table_headers = string_display.get_strings_from_cache(table_headers, app_context)
 
     # paginator is 1-based indexing (yikes)
-    page_number = request.GET.get('page') or 1
     paginator = Paginator(data, RESULTS_PER_PAGE)
-
     subset_data = paginator.page(page_number)
 
     pages = get_pagination_ranges(paginator, int(page_number))
-
     return render(request, 'data_display/database_start_page.html',
                   {'data': subset_data, 'table_headers': table_headers,
                    'pages': pages}
@@ -57,9 +63,18 @@ def get_objects_by_table(table_name):
     }[table_name]
 
 
+def get_objects_by_table_and_sort(table_name, sort_by):
+    return {
+        'Students': (Students.objects.order_by(sort_by).values_list(), Students._meta.get_fields()),
+        'Teams': (Teams.objects.order_by(sort_by).values_list(), Teams._meta.get_fields()),
+        'Projects': (Projects.objects.order_by(sort_by).values_list(), Projects._meta.get_fields()),
+        'Not For Profits': (NotForProfits.objects.order_by(sort_by).values_list(), NotForProfits._meta.get_fields())
+    }[table_name]
+
+
 def get_pagination_ranges(paginator, curr_page):
     total_pages = paginator.num_pages
-    pages = {'left': [], 'right': []}
+    pages = {'left': [], 'right': [], 'current': curr_page}
 
     if curr_page - 1 > 1:
         pages['left'] = [curr_page - 2, curr_page - 1]
